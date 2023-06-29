671313a27c4ebe4e4d92a99d771d36317b62d3fe
FabianCollao <fabian.22xde@gmail.com>
2023-06-29 14:24:55 -0400

    Contacto y Crear Capacitacion

--- src/main/java/servlet/CrearCapacitacion.java
+++ src/main/java/servlet/CrearCapacitacion.java
@@ -1,4 +1,5 @@
 package servlet;
+import model.Capacitacion;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -35,7 +36,24 @@ public class CrearCapacitacion extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		//Obteniendo parametros del formulario
+		int id = Integer.valueOf(request.getParameter("idCapacitacion"));
+		int rutCliente = Integer.valueOf(request.getParameter("rutCliente"));
+		String dia = request.getParameter("dia");
+		String hora = request.getParameter("hora");
+		String lugar = request.getParameter("lugar");
+		String duracion = request.getParameter("duracion");
+		int cantidadAsistentes = Integer.valueOf(request.getParameter("cantidadAsistentes"));
+		
+		//Creando nueva capacitacion
+		Capacitacion capacitacion = new Capacitacion(id,rutCliente,dia,hora,lugar,duracion,cantidadAsistentes);
+		
+		// Establecer el mensaje de éxito como atributo de solicitud
+		request.setAttribute("mensaje", "La Capacitacion se ha agregado correctamente.");
+		
+		//Redireccionar a web de exito
+		getServletContext().getRequestDispatcher("/views/exito.jsp").forward(request, response);
 	}
 
 }
---
+++ src/main/webapp/views/exito.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Éxito</title>
+<!-- CSS Bootstrap -->
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+	crossorigin="anonymous">
+</head>
+<body>
+	<%@ include file='navbar.jsp'%>
+	<div class="container">
+		<div class="alert alert-success mt-4">
+			<h4 class="alert-heading text-center fs-1">¡Éxito!</h4>
+			<p class="text-center">${mensaje}</p>
+		</div>
+	</div>
+	<!-- JavaScript Bootstrap -->
+	<script
+		src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+		crossorigin="anonymous"></script>
+</body>
+</html>
