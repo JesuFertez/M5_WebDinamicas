dbe77c3c4b8833f8375d021232426281ffc03e66
Lelen <ecp.marlene@hotmail.com>
2023-06-30 18:33:50 -0400

    Se agregan pdtes punto 3B y 4

--- src/main/java/servlet/Contacto.java
+++ src/main/java/servlet/Contacto.java
@@ -3,9 +3,11 @@ package servlet;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Contacto
@@ -27,7 +29,14 @@ public class Contacto extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		getServletContext().getRequestDispatcher("/views/contacto.jsp").forward(request, response);
+		
+		HttpSession session = request.getSession();
+	    if (session != null && session.getAttribute("usuario") != null) {
+	    	getServletContext().getRequestDispatcher("/views/contacto.jsp").forward(request, response);
+	    } else {
+	    	response.sendRedirect(request.getContextPath() + "/Login");
+	        // código para desplegar el diseño respectivo
+	    }
 	}
 
 	/**
